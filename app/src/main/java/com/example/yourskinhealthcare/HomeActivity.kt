76795fd8f9e6bce37de381d9f2f49e3a4fca0c0e
Hanvey <hanveyxavero888@gmail.com>
2021-05-28 07:53:19 +0700

    try one

--- app/src/main/java/com/example/yourskinhealthcare/HomeActivity.kt
+++ app/src/main/java/com/example/yourskinhealthcare/HomeActivity.kt
@@ -3,7 +3,10 @@ package com.example.yourskinhealthcare
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import com.example.yourskinhealthcare.main.DetectActivity
+import com.example.yourskinhealthcare.main.NewActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +14,13 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val btn = findViewById<ImageView>(R.id.button1)
+        val coba = findViewById<Button>(R.id.cobaTes)
         btn.setOnClickListener {
             val intent = Intent(this, profile::class.java)
+            startActivity(intent);
+        }
+        coba.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
             startActivity(intent);
         }
     }
---
+++ app/src/main/java/com/example/yourskinhealthcare/main/DetectActivity.java
@@ -0,0 +1,12 @@
+package com.example.yourskinhealthcare.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.yourskinhealthcare.R;
+
+public class DetectActivity extends AppCompatActivity {
+
+
+}
